bd46f9c0956113aef3db440dc43cb2813368e99d
NTimmons <ngtimmons@gmail.com>
2022-09-24 19:16:57 +0100

    Added text output and array output.

--- maddy2409/main.cpp
+++ maddy2409/main.cpp
@@ -1,6 +1,16 @@
-
+#include <iostream>
+#include <vector>
 
 int main()
 {
+	std::cout << "Hello Maddy, this is standard output to the console \n";
+	std::cout << "We use \\n for writing new lines, which puts text on the next line.";
+
+	std::vector<int> anArray = { 0,1,2,3,4,5 };
+	for (int i = 0; i < anArray.size(); i++)
+	{
+		std::cout << "Array Index [" << i << "] -> " << anArray[i] << "\n";
+	}
+
 	return 0;
 }
